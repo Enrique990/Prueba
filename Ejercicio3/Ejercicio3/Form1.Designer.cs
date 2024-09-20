805f805904f1ff62fcbbeece56d862871a4e9ce6
Enrique990 <voidenrique@gmail.com>
2024-09-20 07:43:37 -0600

    Primera parte del ejercicio 3 completada

--- Ejercicio3/Ejercicio3/Form1.Designer.cs
+++ Ejercicio3/Ejercicio3/Form1.Designer.cs
@@ -21,20 +21,85 @@
         }
 
         #region Código generado por el Diseñador de Windows Forms
-
-        /// <summary>
-        /// Método necesario para admitir el Diseñador. No se puede modificar
-        /// el contenido de este método con el editor de código.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewResult = new System.Windows.Forms.DataGridView();
+            this.btnSumar = new System.Windows.Forms.Button();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(258, 12);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // dataGridViewResult
+            // 
+            this.dataGridViewResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewResult.Location = new System.Drawing.Point(504, 12);
+            this.dataGridViewResult.Name = "dataGridViewResult";
+            this.dataGridViewResult.Size = new System.Drawing.Size(240, 150);
+            this.dataGridViewResult.TabIndex = 2;
+            // 
+            // btnSumar
+            // 
+            this.btnSumar.Location = new System.Drawing.Point(12, 168);
+            this.btnSumar.Name = "btnSumar";
+            this.btnSumar.Size = new System.Drawing.Size(75, 23);
+            this.btnSumar.TabIndex = 3;
+            this.btnSumar.Text = "Sumar";
+            this.btnSumar.UseVisualStyleBackColor = true;
+            this.btnSumar.Click += new System.EventHandler(this.btnSumar_Click);
+            // 
+            // btnMultiplicar
+            // 
+            this.btnMultiplicar.Location = new System.Drawing.Point(93, 168);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(75, 23);
+            this.btnMultiplicar.TabIndex = 4;
+            this.btnMultiplicar.Text = "Multiplicar";
+            this.btnMultiplicar.UseVisualStyleBackColor = true;
+            this.btnMultiplicar.Click += new System.EventHandler(this.btnMultiplicar_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(756, 203);
+            this.Controls.Add(this.btnMultiplicar);
+            this.Controls.Add(this.btnSumar);
+            this.Controls.Add(this.dataGridViewResult);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Operaciones con Matrices";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewResult)).EndInit();
+            this.ResumeLayout(false);
         }
 
-        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridViewResult;
+        private System.Windows.Forms.Button btnSumar;
+        private System.Windows.Forms.Button btnMultiplicar;
     }
+
+    #endregion
 }
 
--- Ejercicio3/Ejercicio3/Form1.cs
+++ Ejercicio3/Ejercicio3/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Ejercicio3.models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,61 @@ namespace Ejercicio3
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InicializarDataGridViews()
+        {
+            dataGridView1.ColumnCount = 4;
+            dataGridView1.RowCount = 4;
+            dataGridView2.ColumnCount = 4;
+            dataGridView2.RowCount = 4;
+            dataGridViewResult.ColumnCount = 4;
+            dataGridViewResult.RowCount = 4;
+        }
+
+        private int[,] LeerMatriz(DataGridView dataGridView)
+        {
+            int[,] matriz = new int[4, 4];
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    matriz[i, j] = Convert.ToInt32(dataGridView[j, i].Value);
+                }
+            }
+            return matriz;
+        }
+
+        private void MostrarMatriz(int[,] matriz, DataGridView dataGridView)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    dataGridView[j, i].Value = matriz[i, j];
+                }
+            }
+        }
+
+        private void btnSumar_Click(object sender, EventArgs e)
+        {
+            int[,] matriz1 = LeerMatriz(dataGridView1);
+            int[,] matriz2 = LeerMatriz(dataGridView2);
+            int[,] resultado = Operacion.SumarMatrices(matriz1, matriz2);
+            MostrarMatriz(resultado, dataGridViewResult);
+        }
+
+        private void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            int[,] matriz1 = LeerMatriz(dataGridView1);
+            int[,] matriz2 = LeerMatriz(dataGridView2);
+            int[,] resultado = Operacion.MultiplicarMatrices(matriz1, matriz2);
+            MostrarMatriz(resultado, dataGridViewResult);
         }
     }
 }
